ad06bb11cec5554a2e7b0291e8164c0b836356ba
Cesar Cueva <cueva1487@gmail.com>
2017-06-22 21:44:14 -0500

    Clase 2

---
+++ Prueba.BL/Services/ClaseService.cs
@@ -0,0 +1,28 @@
+﻿using Prueba.DAO.Repositories;
+using Prueba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.BL.Services
+{
+    public class ClaseService
+    {
+        ClaseRepository repository = new ClaseRepository();
+        public ClaseService()
+        {
+
+        }
+
+        public List<Clase> ListarClases()
+        {
+            List<Clase> lista = repository.ListarClases();
+            return lista;
+
+
+        }
+
+    }
+}
